f239e64d41583fcb9ac0fc55962ba8a0c1bf33a4
RADHEK <zhou_hl@outlook.com>
2018-11-27 23:17:44 +0800

    movement fixed

--- New Unity Project (2)/Assets/scripts/Player.cs
+++ New Unity Project (2)/Assets/scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    public float horizontal,verical;
+    public float horizontal,vertical;
     public float Player_Speed;
     public Rigidbody2D Player_rb;
     Vector2 movement;
@@ -17,8 +17,8 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         horizontal = Input.GetAxis("Horizontal");
-        //verical = Input.GetAxis("Verical");
-        Player_rb.velocity = new Vector2(horizontal * Player_Speed, 0);
+        vertical = Input.GetAxis("Vertical");
+        Player_rb.velocity = new Vector2(horizontal * Player_Speed, vertical*Player_Speed);
     }
  
 }
