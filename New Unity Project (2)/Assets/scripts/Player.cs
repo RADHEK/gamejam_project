f0ddfa99daccbe74851647cd0e3d4996932fcda6
RADHEK <zhou_hl@outlook.com>
2018-11-27 23:00:01 +0800

    0.001

---
+++ New Unity Project (2)/Assets/scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float horizontal,verical;
+    public float Player_Speed;
+    public Rigidbody2D Player_rb;
+    Vector2 movement;
+
+	// Use this for initialization
+	void Start () {
+        Player_rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        horizontal = Input.GetAxis("Horizontal");
+        //verical = Input.GetAxis("Verical");
+        Player_rb.velocity = new Vector2(horizontal * Player_Speed, 0);
+    }
+ 
+}
