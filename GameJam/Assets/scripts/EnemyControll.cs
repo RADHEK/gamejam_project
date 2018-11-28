49a731bfc7589756f4cc93e1f2c9d80dcc147719
RADHEK <zhou_hl@outlook.com>
2018-11-28 17:55:48 +0800

    enemy ai
    Now enemy can follow the player.

---
+++ GameJam/Assets/scripts/EnemyControll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControll : MonoBehaviour {
+
+    public Transform Player;
+    public float Enemy_MoveSpeed;
+    public Rigidbody2D Enemy1;
+
+
+	// Use this for initialization
+	void Start () {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+        
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position=Vector2.MoveTowards(transform.position, Player.position, Enemy_MoveSpeed * Time.deltaTime);
+	}
+}
