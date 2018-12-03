4c04fab88eabe637fa07cc486a2f751e4ffdc7de
Ultcrt <zhang_zhe_yuan@outlook.com>
2018-12-03 23:17:54 +0800

    Revert "Revert "enemy ai""
    This reverts commit a8e0d2a97f88b4bf5b232419bfed3b5b678e3c4c.

--- GameJam/Assets/scripts/EnemyControll.cs
+++ GameJam/Assets/scripts/EnemyControll.cs
@@ -2,22 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyControll : MonoBehaviour {
+public class EnemyControll : MonoBehaviour
+{
 
     public Transform Player;
     public float Enemy_MoveSpeed;
     public Rigidbody2D Enemy1;
+    private float Distance;
+    bool IsFacingRight = true;
 
-
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         Player = GameObject.FindGameObjectWithTag("Player").transform;
-        
-		
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate() {
-        transform.position=Vector2.MoveTowards(transform.position, Player.position, Enemy_MoveSpeed * Time.deltaTime);
-	}
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Chase();
+        if (Player.position.x > Enemy1.transform.position.x && !IsFacingRight)
+            Flip();
+        if (Player.position.x < Enemy1.transform.position.x && IsFacingRight)
+            Flip();
+    }
+
+    private void Chase()
+    {
+        Distance = Vector2.Distance(Player.position, Enemy1.transform.position);
+        if (Distance < 500)
+            transform.position = Vector2.MoveTowards(transform.position, Player.position, Enemy_MoveSpeed * Time.deltaTime);
+
+    }
+
+    private void Flip()
+    {
+        IsFacingRight = !IsFacingRight;
+        Vector2 Temp = transform.localScale;
+        Temp.x *= -1;
+        transform.localScale = Temp;
+    }
+
+
 }
+
---
+++ GameJam/Assets/scripts/EnemySpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+    public GameObject Enemy_Prefab;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Input.GetMouseButtonDown(0))
+            Instantiate(Enemy_Prefab, Vector3.zero, Quaternion.identity);
+    }
+}
