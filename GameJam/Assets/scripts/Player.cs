de3d9146c14837d957721b2ef9c026e99b545d29
Ultcrt <zhang_zhe_yuan@outlook.com>
2018-12-05 10:52:02 +0800

    2018.12.5

--- GameJam/Assets/scripts/Player.cs
+++ GameJam/Assets/scripts/Player.cs
@@ -14,6 +14,7 @@ public class Player : MonoBehaviour
     // Use this for initialization
     void Start ()
     {
+       
         Player_rb = GetComponent<Rigidbody2D>();
 	}
     void OnTriggerStay2D(Collider2D collider2D)
@@ -26,9 +27,26 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate ()
     {
+        Animator anim = this.GetComponent<Animator>();
+        if (Input.GetKey(KeyCode.W)|| Input.GetKey(KeyCode.UpArrow))
+        {
+            anim.SetInteger("Decision", 0);
+        }
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            anim.SetInteger("Decision", 2);
+        }
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            anim.SetInteger("Decision", 3);
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            anim.SetInteger("Decision", 1);
+        }
         Move();
-        if (Player_rb.velocity.x > 0 && !IsFacingRight) Flip();
-        if (Player_rb.velocity.x < 0 && IsFacingRight) Flip();
+        //if (Player_rb.velocity.x > 0 && !IsFacingRight) Flip();
+        //if (Player_rb.velocity.x < 0 && IsFacingRight) Flip();
     }
 
     private void Move()
@@ -38,12 +56,12 @@ public class Player : MonoBehaviour
         Player_rb.velocity = new Vector2(horizontal * Player_Speed, vertical * Player_Speed);
     }
 
-    private void Flip()
+    /*private void Flip()
     {
         IsFacingRight = !IsFacingRight;
         Vector2 Temp = transform.localScale;
         Temp.x *= -1;
         transform.localScale = Temp;
-    }
+    }*/
 }
 
